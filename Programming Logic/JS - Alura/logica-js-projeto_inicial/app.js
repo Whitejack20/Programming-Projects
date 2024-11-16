4c5d1f3ea992c6a208baf2b7644f78f8b54a9ea5
Artêmio Andrade Barros <artemio.barros@gmail.com>
2024-11-16 09:49:22 -0300

    Aula 01 - Iniciando com o JavaScript Realocated the files to a parent folder Commented the code to understand the use of the commands

--- Programming Logic/JS - Alura/logica-js-projeto_inicial/app.js
+++ Programming Logic/JS - Alura/logica-js-projeto_inicial/app.js
@@ -1,7 +1,12 @@
+// Display a message for the user
 alert('Boas vindas ao jogo do número secreto!');
+// Variable definition
 let numeroSecreto = 29;
+// Storage and user input
 let chute = prompt('Escolha um número entre 1 e 30')
 
+// Simples Conditional
 if (chute == numeroSecreto) {
+    // Display a message on the Console tab
     console.log("Isso aí! Você descobriu o número secreto")
 }
