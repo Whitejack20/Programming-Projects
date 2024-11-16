e3cc37bab3a59b7ef909f49c25c6a6ee1d7a2d24
Artêmio Andrade Barros <artemio.barros@gmail.com>
2024-11-16 11:16:31 -0300

    Aula 02 - Condicionais e Concatenação Included the else block Concatenated string with variable value Initated the live server

--- Programming Logic/JS - Alura/logica-js-projeto_inicial/app.js
+++ Programming Logic/JS - Alura/logica-js-projeto_inicial/app.js
@@ -1,12 +1,17 @@
 // Display a message for the user
 alert('Boas vindas ao jogo do número secreto!');
 // Variable definition
-let numeroSecreto = 29;
+let numeroSecreto = 9;
+// Display the variable on the Console tab
+console.log(numeroSecreto)
 // Storage and user input
 let chute = prompt('Escolha um número entre 1 e 30')
 
-// Simples Conditional
+// Simple Conditional
 if (chute == numeroSecreto) {
-    // Display a message on the Console tab
-    console.log("Isso aí! Você descobriu o número secreto")
+    // alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}`) // Template Strings
+    alert('Isso aí! Você descobriu o número secreto ' + numeroSecreto) // Alternative
+}
+else {
+    alert('Você errou :(')
 }
