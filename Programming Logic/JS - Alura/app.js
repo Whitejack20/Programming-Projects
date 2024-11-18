d280729390c484d0ad9f20fab9632d9e7dbdaa0b
Artêmio Andrade Barros <artemio.barros@gmail.com>
2024-11-17 23:50:42 -0300

    Aula 03 - Loops e Tentativas Added more conditionals Created a while loop and implemented "tries" mechanic

--- Programming Logic/JS - Alura/app.js
+++ Programming Logic/JS - Alura/app.js
@@ -3,15 +3,29 @@ alert('Boas vindas ao jogo do número secreto!');
 // Variable definition
 let numeroSecreto = 9;
 // Display the variable on the Console tab
-console.log(numeroSecreto)
+console.log(numeroSecreto);
 // Storage and user input
-let chute = prompt('Escolha um número entre 1 e 30')
+let chute;
+let tentativas = 0;
 
-// Simple Conditional
-if (chute == numeroSecreto) {
-    // alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}`) // Template Strings
-    alert('Isso aí! Você descobriu o número secreto ' + numeroSecreto) // Alternative
-}
-else {
-    alert('Você errou :(')
+// While Loop
+while(chute != numeroSecreto) {
+
+    chute = prompt('Escolha um número entre 1 e 30')
+
+    // Simple Conditional
+    if (chute == numeroSecreto) {
+        // alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}`) // Template Strings
+        alert('Isso aí! Você descobriu o número secreto ' + numeroSecreto + ' com ' + tentativas + ' tentativas.'); // Alternative
+    }
+    else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`)
+        }
+        else {
+            alert(`O número secreto é maior que ${chute}`)
+        }
+        tentativas++;
+    }
+
 }
